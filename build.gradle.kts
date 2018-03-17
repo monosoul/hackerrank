1f54f9e88729cf1fb18ba257fb043582f81ac4f3
monosoul <kloz.klaud@gmail.com>
2018-03-17 18:47:04 +0500

    initial gradle project commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+group = "com.hackerrank.monosoul"
+version = "1.0"
+
+plugins {
+    java
+}
+
+apply {
+    plugin("org.junit.platform.gradle.plugin")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.1.0")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.1.0")
+    testCompile("org.assertj:assertj-core:3.9.1")
+}
+
+repositories {
+    jcenter()
+}
+
+buildscript {
+    repositories{
+        jcenter()
+    }
+
+    dependencies{
+        classpath("org.junit.platform:junit-platform-gradle-plugin:1.1.0")
+    }
+}

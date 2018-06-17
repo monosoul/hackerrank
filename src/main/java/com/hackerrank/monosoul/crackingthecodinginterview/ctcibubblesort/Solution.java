294e5d99a4f70247e8f0f3e92f26c2c612251a61
monosoul <kloz.klaud@gmail.com>
2018-06-17 23:35:48 +0500

    initial commit of bubble sort solution

---
+++ src/main/java/com/hackerrank/monosoul/crackingthecodinginterview/ctcibubblesort/Solution.java
@@ -0,0 +1,43 @@
+package com.hackerrank.monosoul.crackingthecodinginterview.ctcibubblesort;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**
+ * https://www.hackerrank.com/challenges/ctci-bubble-sort/problem
+ */
+public class Solution {
+
+    // Complete the countSwaps function below.
+    static void countSwaps(int[] a) {
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        countSwaps(a);
+
+        scanner.close();
+    }
+}
+
